0b57851341b19e44cece7a2a9a34b0da7bb29cad
Rosano Coutinho <github.com@xeari.com>
2025-10-18 09:13:03 +0100

    write local files

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Sample Node.js app that can be [one-click](https://easyindie.app) deployed to self-hosted servers.
 
+Each request writes a file to the local filesystem, which should persist between restarts.
+
 ## Questions
 
 Feel free to reach out on [Mastodon](https://rosano.ca/mastodon) or [Bluesky](https://rosano.ca/bluesky).
--- main.js
+++ main.js
@@ -1,11 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
 const port = process.env.PORT || 3000;
 
 require('http').createServer((req, res) => {
   const date = new Date();
+
+  const destination = path.join(process.env.DATA_DIRECTORY || __dirname, '__local', date.valueOf() + '.txt');
+
+  if (!fs.existsSync(path.dirname(destination))){
+    fs.mkdirSync(path.dirname(destination));
+  }
+
+  fs.writeFileSync(destination, '');
+
   res.writeHead(200, {
     'Content-Type': 'text/plain',
   });
-  res.end('Hello!\n\nRequest time: ' + date.toJSON());
+  res.end([
+    'Hello!',
+    'Request time: ' + date.toJSON(),
+    'Local Files: ' + fs.readdirSync(path.dirname(destination)).filter(e => e.endsWith('.txt')).length,
+  ].join('\n\n'));
 }).listen(port);
 
 console.log(`Listening on port ${ port }`);
