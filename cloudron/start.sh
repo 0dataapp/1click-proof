176e6fe9a73a0b5d0c0b69dd0e8813cfa0b63fe5
Rosano Coutinho <github.com@xeari.com>
2025-10-20 12:33:01 +0100

    fix typo

--- cloudron/start.sh
+++ cloudron/start.sh
@@ -2,7 +2,8 @@
 
 # ensure that this file executable in the app repo: chmod +x start.sh.
 
+chown -R cloudron:cloudron /app/data
+
 mkdir -p /app/data/__local
-chown -R cloudron:cloudron /app/data/__local/
 
 exec /usr/local/bin/gosu cloudron:cloudron npm start
